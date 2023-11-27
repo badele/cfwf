ce203535c2f31ec80ccdf570999cf95330e0456c
Bruno Adelé <brunoadele@gmail.com>
2023-11-27 08:15:57 +0100

    chore: change release notation

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 TAGVERSION=$1
-VERSION="${TAGVERSION:1}"
 
 cog changelog --at "$TAGVERSION" -t full_hash > GITHUB_CHANGELOG.md
-sed -i "s/\".*\"/\"$VERSION\"/" src/version.ts
-sed -i "s/\@[0-9]\.[0-9]\.[0-9]/@$VERSION/" README.md
 
+# Version
+sed -i "s/\".*\"/\"$TAGVERSION\"/" src/version.ts
+
+# Readme
+sed -i "s/\@[0-9]\.[0-9]\.[0-9]/@$TAGVERSION/" README.md
+sed -i "s/Version\: [0-9]\.[0-9]\.[0-9]/Version: $TAGVERSION/" README.md
